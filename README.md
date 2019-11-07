16615fbedf0ef239aa497e2c45c5825271f38cce
PARAM MITTAL <parammittal16@gmail.com>
2019-11-07 14:46:18 +0000

    Add python-runner

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mini-hospital-api
+
+[![Codeship Status for parammittal16/mini-hospital-api](https://app.codeship.com/projects/546ef180-e381-0137-ae3c-72f0dac3b5c0/status?branch=master)](https://app.codeship.com/projects/373050)
--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+var bodyParser = require('body-parser');
+let {PythonShell} = require('python-shell');
 
-app.get('/', (req, res) => res.send('Hello2 World!'))
+const app = express();
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(bodyParser.json());
+
+app.get('/sample', (req, res) => {
+  var options = {
+    args:
+    [
+      req.query.funds, // starting funds
+      req.query.size, // (initial) wager size
+      req.query.count, // wager count — number of wagers per sim
+      req.query.sims // number of simulations
+    ]
+  }
+  console.log("------>", options);
+  PythonShell.run('./python-scripts/sample.py', options, function (err, data) {
+    if (err) res.send(err);
+    console.log(data.toString());
+    res.send(data.toString())
+  });
+});
+
+app.get('/', (req, res) => res.send('Hello2 World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
