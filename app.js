1bb053a010cc8843415ab2595f92bcc934ecf360
parammittal162 <param.mittal@chqbook.com>
2020-05-02 18:28:13 +0530

    Add image save support

--- app.js
+++ app.js
@@ -1,55 +1,90 @@
 /*eslint-env es6*/
 const express = require('express');
-var bodyParser = require('body-parser');
-let { PythonShell } = require('python-shell');
+const bodyParser = require('body-parser');
+const { PythonShell } = require('python-shell');
+const multer = require('multer');
+
 
 const app = express();
 const port = 3000;
 
+
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  //set headers to allow cross origin request.
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/brain', (req, res) => {
+
+app.post('/brain', (req, res) => {
   const brain_path = __dirname + '/python-scripts/brain-tumor-detection';
-  var options = {
-    args:
-      [
-        brain_path + '/data/' + req.query.img,
-        brain_path + '/cnn-parameters-improvement-23-0.91.model'
-      ]
-  }
-  console.log("------>", options);
-  PythonShell.run(brain_path + '/predict.py', options, function (err, data) {
+  const upload = multer({ dest: brain_path + '/data/' }).single('photo');
+  upload(req, res, function (err) {
     if (err) {
       console.log(err);
-      res.send(err);
+      return res.status(422).send("an Error occured")
     }
-    console.log(data);
-    res.send(data)
+    const options = {
+      args:
+        [
+          req.file.path,
+          brain_path + '/cnn-parameters-improvement-23-0.91.model'
+        ]
+    }
+    console.log("------>", options);
+    PythonShell.run(brain_path + '/predict.py', options, function (err, data) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      }
+      console.log(data);
+      res.send(data)
+    });
   });
 });
 
-app.get('/bone', (req, res) => {
+app.post('/bone', (req, res) => {
   const bone_path = __dirname + '/python-scripts/bone-fracture-detection';
-  var options = {
-    args:
-      [
-        bone_path + '/data/' + req.query.img,
-        bone_path + '/model.h5'
-      ]
-  }
-  console.log("------>", options);
-  PythonShell.run(bone_path + '/predict.py', options, function (err, data) {
+  const upload = multer({ dest: bone_path + '/data/' }).single('photo');
+  upload(req, res, function (err) {
     if (err) {
       console.log(err);
-      res.send(err);
+      return res.status(422).send("an Error occured")
     }
-    console.log(data);
-    res.send(data)
+    var options = {
+      args:
+        [
+          req.file.path,
+          bone_path + '/model.h5'
+        ]
+    }
+    console.log("------>", options);
+    PythonShell.run(bone_path + '/predict.py', options, function (err, data) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      }
+      console.log(data);
+      res.send(data)
+    });
   });
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Mini Hospital API'));
+
+// app.post('/post', function (req, res) {
+//   upload(req, res, function (err) {
+//     if (err) {
+//       console.log(err);
+//       return res.status(422).send("an Error occured")
+//     }
+//     return res.send("Upload Completed for " + req.file.path);
+//   });
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-//python3 predict.py '/home/ubuntu18/Documents/My/00 PROJECTS/4rth YEAR PROJECT/mini-hospital-api/python-scripts/bone-fracture-detection/bone.png'
+// python3 predict.py '/home/ubuntu18/Documents/My/00 PROJECTS/4rth YEAR PROJECT/mini-hospital-api/python-scripts/bone-fracture-detection/bone.png'
